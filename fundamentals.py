201db25532ff2a461d82429d73e37bb54fa1f6e9
wimp777 <11262701+wimp777@users.noreply.github.com>
2023-04-28 08:38:10 -0500

    commit

---
+++ fundamentals.py
@@ -0,0 +1,26 @@
+# python uses sname_case for variables
+# python uses camelCase for functions
+# python uses PascalCase for classes
+
+print(10)
+print(-3000)
+
+# assign interger to variable
+num = 123456
+print(num)
+
+# floating point number
+print(1.0000000000005)  # positive float
+print(-85.34521234)  # negartive float
+
+float_num = 1.0000000000005
+print(float_num)
+
+
+#
+# Boolean
+#
+
+print(True)
+f_bool = False
+print(f_bool)
